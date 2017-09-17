29ad040a47473bfa00fb2f4e8fc1b96bf7da52ee
Vuong Dang <ddvuong@gmail.com>
2017-09-17 21:53:13 +0700

    adfadsfasf

--- src/main/java/com/study/basic/HelloWorld.java
+++ src/main/java/com/study/basic/HelloWorld.java
@@ -4,5 +4,13 @@ public class HelloWorld {
 
     public void main(String[] args) {
         System.out.println("Hello world" );
+
+        System.out.println("Result1: " + add(1, 2));
+        System.out.println("Result2: " + add(3, 4));
+
+    }
+
+    private int add(int a, int b) {
+        return a + b;
     }
 }
