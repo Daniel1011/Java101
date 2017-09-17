8cfa6911c8fb4b6bcab9750a2c5ec5b5e8968b1f
Vuong Dang <ddvuong@gmail.com>
2017-09-17 21:36:55 +0700

    Fix bug ABC

--- src/main/java/com/study/basic/HelloWorld.java
+++ src/main/java/com/study/basic/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public void main(String[] args) {
         System.out.println("Hello world" );
 
+        // currently this is the only way to print out
         System.out.println("End." );
     }
 }
